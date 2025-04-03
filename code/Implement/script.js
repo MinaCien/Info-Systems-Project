da36241ed264c7e8331d50c5f75c47b46225eaef
Wilhelmina Acheampong <124389092+MinaCien@users.noreply.github.com>
2025-04-03 14:03:38 +0100

    Delete code/Implement/script.js

--- code/Implement/script.js
+++
@@ -1,9 +0,0 @@
-
-function showSection(sectionId) {
-    document.getElementById("first").style.display = (sectionId === "first") ? "block" : "none";
-    document.getElementById("second").style.display = (sectionId === "second") ? "block" : "none";
-}
-
-function removeBookmark(button) {
-    button.parentElement.remove();
-}
